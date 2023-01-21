bf842faae83be294f571e30c9838d83a9247efe3
FrancisVega <hisconer@gmail.com>
2023-01-21 17:59:31 +0100

    fix audit problems

--- fzfnpm.rb
+++ fzfnpm.rb
@@ -1,18 +1,15 @@
 class Fzfnpm < Formula
-  desc "A fuzzy finder npm script runner"
+  desc "Fuzzy finder npm script runner"
   homepage "https://github.com/FrancisVega/fzfnpm"
   url "https://github.com/FrancisVega/fzfnpm/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "883cf5df0a76abbd22726609d8f516134e23350bfcb7fee5f2d29b1a5a0dc147"
 
   license "MIT"
 
-  depends_on "bash" => :run
-  depends_on "fzf" => :run
+  depends_on "bash"
+  depends_on "fzf"
 
   def install
     bin.install "bin/fzfnpm"
-  end
-
-  test do
   end
 end
