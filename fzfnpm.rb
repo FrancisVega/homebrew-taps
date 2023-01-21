d47493e55e21a8c8cfbb0ff9ef18913249cc99bc
FrancisVega <hisconer@gmail.com>
2023-01-21 17:40:15 +0100

    update

--- fzfnpm.rb
+++ fzfnpm.rb
@@ -1,15 +1,16 @@
 class Fzfnpm < Formula
   desc "A fuzzy finder npm script runner"
   homepage "https://github.com/FrancisVega/fzfnpm"
-  url "https://github.com/FrancisVega/fzfnpm/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "c5c5f1faae3eb7f6c8c376d7d0f9a09c7a25da1a0be9fc28cd9ff6ef4d9862b5"
+  url "https://github.com/FrancisVega/fzfnpm/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "bb1fdea62ad430fe0ed4c6c26d3c7f1b0ab057e843fd1332596ddf8a5c1a27df"
+
   license "MIT"
 
   depends_on "bash" => :run
   depends_on "fzf" => :run
 
   def install
-    bin.install "bin/fzfnpm.sh"
+    bin.install "bin/fzfnpm"
   end
 
   test do
